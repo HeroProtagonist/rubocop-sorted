7389cc5dc0d26ca9fe4b2bb26b2de13382fae9a8
HeroProtagonist <jstepper91@gmail.com>
2021-12-17 13:59:07 -0500

    Add documentation comments

--- lib/rubocop/cop/sorted/sort_hash_keys.rb
+++ lib/rubocop/cop/sorted/sort_hash_keys.rb
@@ -3,44 +3,22 @@
 module RuboCop
   module Cop
     module Sorted
-      # TODO: Write cop description and example of bad / good code. For every
-      # `SupportedStyle` and unique configuration, there needs to be examples.
-      # Examples must have valid Ruby syntax. Do not use upticks.
-      #
-      # @safety
-      #   Delete this section if the cop is not unsafe (`Safe: false` or
-      #   `SafeAutoCorrect: false`), or use it to explain how the cop is
-      #   unsafe.
-      #
-      # @example EnforcedStyle: bar (default)
-      #   # Description of the `bar` style.
+      # Checks that the keys of a hash are sorted alphabetically when enabled.
+      # The configuration option `VariableNamesToCheck` can be set to an array
+      # of strings. Only hashes assigned to variables matching the supplied
+      # names will be inspected. By default, this option is not set.
       #
       #   # bad
-      #   bad_bar_method
-      #
-      #   # bad
-      #   bad_bar_method(args)
-      #
-      #   # good
-      #   good_bar_method
+      #   hash = {
+      #     z: 1,
+      #     a: 2
+      #   }
       #
       #   # good
-      #   good_bar_method(args)
-      #
-      # @example EnforcedStyle: foo
-      #   # Description of the `foo` style.
-      #
-      #   # bad
-      #   bad_foo_method
-      #
-      #   # bad
-      #   bad_foo_method(args)
-      #
-      #   # good
-      #   good_foo_method
-      #
-      #   # good
-      #   good_foo_method(args)
+      #   hash = {
+      #     a: 2,
+      #     z: 1
+      #   }
       #
       class SortHashKeys < Base
         extend AutoCorrector
